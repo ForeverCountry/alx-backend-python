8ab322c6c9a0dc939a1200f843247bda2d5778bd
ForeverCountry <40751841+ForeverCountry@users.noreply.github.com>
2024-12-12 14:40:14 +0200

    update 3-concurrent.py

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -1,26 +1,27 @@
 import asyncio
 import aiosqlite
 
+DB_NAME = "User_data.db"
 
-async def async_fetch_users(db_name):
+
+async def async_fetch_users():
     """Fetch all users from the database."""
-    async with aiosqlite.connect(db_name) as db:
+    async with aiosqlite.connect(DB_NAME) as db:
         async with db.execute("SELECT * FROM users") as cursor:
             return await cursor.fetchall()
 
 
-async def async_fetch_older_users(db_name):
+async def async_fetch_older_users():
     """Fetch users older than 40 from the database."""
-    async with aiosqlite.connect(db_name) as db:
+    async with aiosqlite.connect(DB_NAME) as db:
         async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
             return await cursor.fetchall()
 
 
 async def fetch_concurrently():
     """Run both queries concurrently."""
-    db_name = "User_data.db"
     results = await asyncio.gather(
-        async_fetch_users(db_name), async_fetch_older_users(db_name)
+        async_fetch_users(), async_fetch_older_users()
     )
     all_users, older_users = results
     print("All Users:", all_users)
