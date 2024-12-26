c57c1f98885fc52adc20a5b85e723648262b741c
ForeverCountry <40751841+ForeverCountry@users.noreply.github.com>
2024-12-26 21:41:00 +0200

    fix: fixed error when running migration in `settings.py`
    - Added the following to `settings.py`: AUTH_USER_MODEL="chats.CustomUser"

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "chats",
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "chats.CustomUser"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.BasicAuthentication",
