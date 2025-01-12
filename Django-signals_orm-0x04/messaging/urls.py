38906d7bfa6efe61026c04c1143e609450e630cc
ForeverCountry <40751841+ForeverCountry@users.noreply.github.com>
2025-01-12 17:47:55 +0200

    add Django-signals_orm-0x04 project files

---
+++ Django-signals_orm-0x04/messaging/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    NotificationViewSet,
+    DeleteUserView,
+)
+from .views import UnreadMessagesView
+from .views import ThreadedConversationView
+
+router = DefaultRouter()
+router.register(r"notifications", NotificationViewSet, basename="notification")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("delete-account/", DeleteUserView.as_view(), name="delete_account"),
+    path(
+        "conversations/<int:conversation_id>/",
+        ThreadedConversationView.as_view(),
+        name="threaded_conversation",
+    ),
+    path(
+        "messages/unread/",
+        UnreadMessagesView.as_view(),
+        name="unread_messages",
+    ),
+]
